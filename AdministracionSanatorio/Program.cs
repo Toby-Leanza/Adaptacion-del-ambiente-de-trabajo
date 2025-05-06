71508f688fe17db85d015e2c2a830f76bff72268
mgonzalesips <142123784+mgonzalesips@users.noreply.github.com>
2025-05-06 12:07:02 -0300

    Add files via upload

---
+++ AdministracionSanatorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdministracionSanatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
